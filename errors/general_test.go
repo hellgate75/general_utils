080db393914fa18a5c423e5434a49a4b72f5f8bb
hellgate75 <hellgate75@gmail.com>
2019-06-08 17:55:12 +0100

    adding new test case in errors

--- errors/general_test.go
+++ errors/general_test.go
@@ -1,6 +1,8 @@
 package errors
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 )
 
@@ -8,6 +10,19 @@ func TestGeneralException(T *testing.T) {
 	text := "test exception"
 	exc := New(text)
 	if exc.Error() != text {
-		T.Fatal("Base Exception instance failed - Expected <" + text + "> but Given <" + exc.Error() + ">")
+		T.Fatal(fmt.Sprintf("Base Exception instance failed - Expected <%s> but Given <%s>", text, exc.Error()))
+	}
+}
+
+func TestIsError(T *testing.T) {
+	text := "test exception"
+	flag := IsError(text)
+	if flag {
+		T.Fatal(fmt.Sprintf("String is not and error - Expected <%t> but Given <%t>", false, flag))
+	}
+	err := errors.New("My Custom Error")
+	flag = IsError(err)
+	if !flag {
+		T.Fatal(fmt.Sprintf("Err is and error - Expected <%t> but Given <%t>", true, flag))
 	}
 }
